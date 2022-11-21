4d584051706f02eab886d9ecf1a87d06f2a06d2e
Milli-saved <million12.tenkir@gmail.com>
2022-11-21 15:48:25 +0300

    login page is underway.

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,11 +1,79 @@
+import { Box, Button, Grid, TextField } from "@mui/material";
 import React from "react";
+import { Formik } from "formik";
+import * as yup from "yup";
+
 import Header from "../../components/Header";
+import Empowering from "../../Images/empowering.png";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+const dataSchema = yup.object().shape({
+  email: yup.string().email().required("Required."),
+  password: yup.string().required("Required."),
+});
 
 function Login() {
+  const onSubmitHandler = (values) => {
+    console.log(values);
+  };
+
   return (
-    <div>
-      <Header title="Login page" subtitle="this is the login page" />
-    </div>
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6}>
+          <Box style={{ border: "2px solid", borderRadius:"40px" }} marginTop="140px" padding="40px">
+            <Header title="Login" />
+            <Formik
+              onSubmit={onSubmitHandler}
+              initialValues={initialValues}
+              validationSchema={dataSchema}
+            >
+              {({ values, errors, handleBlur, handleChange, handleSubmit }) => (
+                <form onSubmit={handleSubmit}>
+                  <Box m="20px" display="grid" gap="30px">
+                    <TextField
+                      fullWidth
+                      variant="outlined"
+                      type="text"
+                      label="Email"
+                      name="email"
+                      onChange={handleChange}
+                      value={values.email}
+                      error={!!errors.email}
+                      helperText={errors.email}
+                      sx={{ gridColumn: "span 4" }}
+                    />
+                    <TextField
+                      fullWidth
+                      variant="outlined"
+                      type="password"
+                      label="Password"
+                      name="password"
+                      onChange={handleChange}
+                      value={values.password}
+                      error={!!errors.password}
+                      helperText={errors.password}
+                      sx={{ gridColumn: "span 4" }}
+                    />
+                  </Box>
+                  <Box display="flex" justifyContent="center" mt="20px">
+                    <Button type="submit" color="primary" variant="outlined">
+                      Login
+                    </Button>
+                    <Button variant="">Forgot Password</Button>
+                  </Box>
+                </form>
+              )}
+            </Formik>
+          </Box>
+        </Grid>
+        <Grid item xs={3}></Grid>
+      </Grid>
+    </Box>
   );
 }
 
